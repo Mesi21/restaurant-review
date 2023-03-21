1d4dcb5a912bbc38ecb18b45711a2ce64316ff87
Mesi <mse212002@gmail.com>
2023-03-21 13:00:09 +0200

    :construction: generate restaurants controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  resources :restaurants
   get 'pages/about'
   get 'pages/contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "restaurants#index"
 end
